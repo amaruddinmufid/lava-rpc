6d06de858dabd034bbde03367c2893a832d54a6b
Amaruddin Mufid <mufidamar@gmail.com>
2024-04-10 08:14:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet.lava.build/lava-referer-f0fc46d2-1bb7-46c6-bff3-4d39a0806e79/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f0fc46d2-1bb7-46c6-bff3-4d39a0806e79/)
 
 
 echo "$curl_output" >> Update.tsx
